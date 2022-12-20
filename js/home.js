b338d970b10ff3a0133ed85ff025287450316401
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2022-12-20 16:22:29 +0100

    Save Value

--- js/home.js
+++ js/home.js
@@ -41,6 +41,7 @@ let zeichensatz = [
   "7",
   "8",
   "9",
+  // ab da anfassen
   "!",
   "?",
   "|",
@@ -52,10 +53,18 @@ function validate() {
   if (!validateAnrede()) return false;
   if (!validateVorname()) return false;
   if (!validateNachname()) return false;
-  /* if (!validateStrasse()) return false; */
+  if (!validateAge()) return false;
   if (!validateStrasse()) return false;
   if (!validatePlz()) return false;
   if (!validateOrt()) return false;
+  if (!validateEmail()) return false;
+  if (!validateTelM()) return false;
+  if (!validateTelP()) return false;
+  if (!validateKursort()) return false;
+  if (!validateKurssprache()) return false;
+  if (!validateUebersetzung()) return false;
+  if (!validateAgb()) return false;
+  if (!validateAnnulirung()) return false;
 
   return true;
 }
@@ -72,6 +81,7 @@ function validateAnrede() {
     )
   ) {
     document.getElementById("anredeFehlemeldung").innerText = "Bitte Ausfüllen";
+    jump("anredeFehlemeldung");
     return false;
   } else {
     let radioButtons = document.getElementsByName("anrede");
@@ -96,6 +106,7 @@ function validateVorname() {
   if (!value) {
     document.getElementById("vornameFehlemeldung").innerText =
       "Bitte Ausfüllen";
+    jump("vornameFehlemeldung");
     return false;
   } else if (
     !sonderzeichenDont(
@@ -106,11 +117,12 @@ function validateVorname() {
       "vornameFehlemeldung"
     )
   ) {
+    jump("vornameFehlemeldung");
     return false;
   } else {
     document.getElementById("vornameFehlemeldung").innerText =
       "du hast " + value + " ausgefult";
-    return false;
+    return true;
   }
 }
 function validateNachname() {
@@ -119,6 +131,7 @@ function validateNachname() {
   if (!value) {
     document.getElementById("nachnameFehlemeldung").innerText =
       "Bitte Ausfüllen";
+    jump("nachnameFehlemeldung");
     return false;
   } else if (
     !sonderzeichenDont(
@@ -129,11 +142,31 @@ function validateNachname() {
       "nachnameFehlemeldung"
     )
   ) {
-    return false;
+    jump("nachnameFehlemeldung");
+    return trfalseue;
   } else {
     document.getElementById("nachnameFehlemeldung").innerText =
       "du hast " + value + " ausgefult";
+    return true;
+  }
+}
+function validateAge() {
+  let inputElement = document.getElementById("geburtsdatum");
+  let value = inputElement.value;
+  if (!value) {
+    document.getElementById("geburtsdatumFehlemeldung").innerText =
+      "Bitte Ausfüllen";
+    jump("geburtsdatumFehlemeldung");
     return false;
+  } else if (value < 16) {
+    document.getElementById("geburtsdatumFehlemeldung").innerText =
+      "jüngar als 16";
+    jump("geburtsdatumFehlemeldung");
+    return false;
+  } else {
+    document.getElementById("geburtsdatumFehlemeldung").innerText =
+      "So alt bist du:" + value;
+    return true;
   }
 }
 function validateStrasse() {
@@ -144,14 +177,14 @@ function validateStrasse() {
       "Bitte Ausfüllen";
     return false;
   } else if (
-    !sonderzeichenDont(0, 2, value, "darf kein (", "strasseFehlemeldung")
+    !sonderzeichenDont(0, 1, value, "darf kein (", "strasseFehlemeldung")
   ) {
     return false;
-  } /* else if (
-    !sonderzeichenMust(2, 11, value, " muss z.B. (", "strasseFehlemeldung")
+  } else if (
+    !sonderzeichenDo(2, 12, value, "muss ein (", "strasseFehlemeldung")
   ) {
     return false;
-  } */ else if (
+  } else if (
     !sonderzeichenDont(
       12,
       zeichensatz.length,
@@ -219,9 +252,9 @@ function validateEmail() {
     )
   ) {
     return false;
-  } /* else if(){
-    abfrage  @.
-  } */ else {
+  } else if (!sonderzeichenDo(0, 2, value, "muss ein (", "eMailFehlemeldung")) {
+    return false;
+  } else {
     document.getElementById("eMailFehlemeldung").innerText =
       "du hast " + value + " ausgefult";
     return true;
@@ -231,7 +264,7 @@ function validateTelM() {
   let inputElement = document.getElementById("telM");
   let value = inputElement.value;
   if (!value) {
-    document.getElementById("telMFehlemeldung").innerText = "Bitte ausfüllen"
+    document.getElementById("telMFehlemeldung").innerText = "Bitte ausfüllen";
     return false;
   }
   document.getElementById("telMFehlemeldung").innerText =
@@ -242,7 +275,7 @@ function validateTelP() {
   let inputElement = document.getElementById("telP");
   let value = inputElement.value;
   if (!value) {
-    document.getElementById("telPFehlemeldung").innerText = "Bitte ausfüllen"
+    document.getElementById("telPFehlemeldung").innerText = "Bitte ausfüllen";
     return false;
   }
   document.getElementById("telPFehlemeldung").innerText =
@@ -251,7 +284,7 @@ function validateTelP() {
 }
 function validateKursort() {
   let radoElementZürich = document.getElementById("zürichKursort");
-  let radoElementParis= document.getElementById("parisKursort");
+  let radoElementParis = document.getElementById("parisKursort");
   let radoElementNewYork = document.getElementById("newYorkKursort");
 
   if (
@@ -261,7 +294,8 @@ function validateKursort() {
       radoElementNewYork.checked
     )
   ) {
-    document.getElementById("kursortFehlemeldung").innerText = "Bitte Ausfüllen";
+    document.getElementById("kursortFehlemeldung").innerText =
+      "Bitte Ausfüllen";
     return false;
   } else {
     let radioButtons = document.getElementsByName("kursort");
@@ -280,6 +314,125 @@ function validateKursort() {
     return true;
   }
 }
+
+function validateKurssprache() {
+  let radoElementGemeinesprache = document.getElementById(
+    "gemeinespracheKurssprache"
+  );
+  let radoElementMensch = document.getElementById("menschKurssprache");
+  let radoElementHalbling = document.getElementById("halblingKurssprache");
+  let radoElementZwergisch = document.getElementById("zwergischKurssprache");
+  let radoElementRiesisch = document.getElementById("riesischKurssprache");
+  let radoElementGnomisch = document.getElementById("gnomischKurssprache");
+  let radoElementGoblinisch = document.getElementById("goblinischKurssprache");
+  let radoElementOrks = document.getElementById("orksKurssprache");
+  let radoElementElfisch = document.getElementById("elfischKurssprache");
+  let radoElementSylvanisch = document.getElementById("sylvanischKurssprache");
+  let radoElementGemeinespracheDerUnterreicheKurssprache =
+    document.getElementById("gemeinespracheDerUnterreicheKurssprache");
+
+  if (
+    !(
+      radoElementGemeinesprache.checked ||
+      radoElementMensch.checked ||
+      radoElementHalbling.checked ||
+      radoElementZwergisch.checked ||
+      radoElementRiesisch.checked ||
+      radoElementGnomisch.checked ||
+      radoElementGoblinisch.checked ||
+      radoElementOrks.checked ||
+      radoElementElfisch.checked ||
+      radoElementSylvanisch.checked ||
+      radoElementGemeinespracheDerUnterreicheKurssprache.checked
+    )
+  ) {
+    document.getElementById("kursspracheFehlemeldung").innerText =
+      "Bitte Ausfüllen";
+    return false;
+  } else {
+    let radioButtons = document.getElementsByName("kurssprache");
+    let value;
+
+    for (let b = 0; b < radioButtons.length; ++b) {
+      let button = radioButtons[b];
+
+      if (button.checked) {
+        value = button.value;
+        document.getElementById("kursspracheFehlemeldung").innerText =
+          "Du hast " + value + " gewält.";
+      }
+    }
+    return true;
+  }
+}
+function validateUebersetzung() {
+  let radoElementDeutsch = document.getElementById("uebersetztDeutsch");
+  let radoElementFranzösich = document.getElementById("uebersetztFranzösich");
+  let radoElementEnglisch = document.getElementById("uebersetztEnglisch");
+
+  if (
+    !(
+      radoElementDeutsch.checked ||
+      radoElementFranzösich.checked ||
+      radoElementEnglisch.checked
+    )
+  ) {
+    document.getElementById("uebersetztFehlemeldung").innerText =
+      "Bitte Ausfüllen";
+    return false;
+  } else {
+    let radioButtons = document.getElementsByName("uebersetzt");
+    let value;
+
+    for (let b = 0; b < radioButtons.length; ++b) {
+      let button = radioButtons[b];
+
+      if (button.checked) {
+        value = button.value;
+        break;
+      }
+    }
+    document.getElementById("uebersetztFehlemeldung").innerText =
+      "Du hast " + value + " gewält.";
+    return true;
+  }
+}
+function validateAgb() {
+  let radoElementAgb = document.getElementById("agb");
+
+  if (!radoElementAgb.checked) {
+    document.getElementById("agbFehlemeldung").innerText = "Bitte Ausfüllen";
+    return false;
+  } else {
+    document.getElementById("agbFehlemeldung").innerText = "Cool";
+    return true;
+  }
+}
+function validateAnnulirung() {
+  let radoElementJa = document.getElementById("jaVersicherung");
+  let radoElementNein = document.getElementById("neinVersicherung");
+
+  if (!(radoElementJa.checked || radoElementNein.checked)) {
+    document.getElementById("versicherungFehlemeldung").innerText =
+      "Bitte Ausfüllen";
+    return false;
+  } else {
+    let radioButtons = document.getElementsByName("versicherung");
+    let value;
+
+    for (let b = 0; b < radioButtons.length; ++b) {
+      let button = radioButtons[b];
+
+      if (button.checked) {
+        value = button.value;
+        break;
+      }
+    }
+    document.getElementById("versicherungFehlemeldung").innerText =
+      "Du hast " + value + " gewält.";
+    return true;
+  }
+}
 function sonderzeichenDont(start, stop, value, toDo, id) {
   for (let z = start; z < stop; ++z) {
     if (value.indexOf(zeichensatz[z]) > 0) {
@@ -289,5 +442,35 @@ function sonderzeichenDont(start, stop, value, toDo, id) {
     }
   }
   return true;
+}
+function sonderzeichenDo(start, stop, value, toDo, id) {
+  console.log("sonderzeichenDo");
+  for (let z = start; z < stop; ++z) {
+    if (value.indexOf(zeichensatz[z]) === -1) {
+      document.getElementById(id).innerText =
+        "Es " + toDo + zeichensatz[z] + ") enthalten!";
+      return false;
+    }
+  }
+  return true;
+}
+
+/* Sprünge */
+function jump(etikette) {
+  var url = location.href;
+  location.href = "#" + etikette;
+  history.replaceState(null, null, url);
+}
+/* Diealeckte */
+function mensch() {
+  let mensch = document.getElementById("menschKurssprache");
+  if (!mensch.checket) {
+    alert("Mensch schaltet ein");
+    gemeinesprache = document.getElementById("gemeinespracheKurssprache");
+    if (!Gemeinesprache.checked) {
+      alert("gemeinesprache ist aus");
+      Gemeinesprache = true;
+    }
+  }
 }
 /* funktionen für Rüchkgabe page */
